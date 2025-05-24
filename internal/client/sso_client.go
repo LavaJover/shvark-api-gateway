7cdf7b7bd01cee80aee06cec924aa9f6c079aacd
bodya <lavajover@gmail.com>
2025-05-24 16:52:45 +0300

    feat: add sso client grpc handlers

---
+++ internal/client/sso_client.go
@@ -0,0 +1,77 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	ssopb "github.com/LavaJover/shvark-sso-service/proto/gen"
+	"google.golang.org/grpc"
+)
+
+type SSOClient struct {
+	conn *grpc.ClientConn
+	service ssopb.SSOServiceClient
+}
+
+func NewSSOClient(addr string) (*SSOClient, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		addr,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SSOClient{
+		conn: conn,
+		service: ssopb.NewSSOServiceClient(conn),
+	}, err
+}
+
+func (c *SSOClient) Register(login, username, rawPassword string) (*ssopb.RegisterResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return c.service.Register(ctx, &ssopb.RegisterRequest{
+		Login: login,
+		Username: username,
+		Password: rawPassword,
+	})
+}
+
+func (c *SSOClient) Login(login, rawPassword string) (*ssopb.LoginResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return c.service.Login(ctx, &ssopb.LoginRequest{
+		Login: login,
+		Password: rawPassword,
+	})
+}
+
+func (c *SSOClient) ValidateToken(token string) (*ssopb.ValidateTokenResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return c.service.ValidateToken(ctx, &ssopb.ValidateTokenRequest{
+		AccessToken: token,
+	})
+}
+
+func (c *SSOClient) GetUserByToken(token string) (*ssopb.GetUserByTokenResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return c.service.GetUserByToken(ctx, &ssopb.GetUserByTokenRequest{
+		AccessToken: token,
+	})
+}
+
+func (c *SSOClient) Close() {
+	c.conn.Close()
+}
---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,22 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+type UserHandler struct {
+
+}
+
+func (h *UserHandler) GetUser(c *gin.Context) {
+}
+
+func (h *UserHandler) CreateUser(c *gin.Context) {
+
+}
+
+func (h *UserHandler) UpdateUser(c *gin.Context) {
+
+}
+
+func (h *UserHandler) DeleteUser(c *gin.Context) {
+	
+}
