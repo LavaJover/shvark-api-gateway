bd2c443112a1edbcf2cd9aa2132bce745a69ab26
bodya <lavajover@gmail.com>
2025-05-24 23:26:30 +0300

    feat: add recovery middleware

--- internal/middleware/recovery.go
+++ internal/middleware/recovery.go
@@ -1,1 +1,19 @@
 package middleware
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RecoveryMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        defer func() {
+            if err := recover(); err != nil {
+                log.Printf("Panic: %v", err)
+                c.AbortWithStatusJSON(500, gin.H{"error": "internal server error"})
+            }
+        }()
+        c.Next()
+    }
+}
